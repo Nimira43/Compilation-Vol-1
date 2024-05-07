a37a2d6fab3f77d7d15c922cb07e5f29cd732382
Nick Rabone <n.rabone72@aol.com>
2024-05-07 17:45:40 +0100

    Progress bar fixed

--- components/Progress Bar/script.js
+++ components/Progress Bar/script.js
@@ -1,6 +1,6 @@
 const progress = document.querySelector('.progress-done')
 
 setTimeout(() => {
-    progress.computedStyleMap.width = progress.getAttribute('data-done') + '%'
-    progress.computedStyleMap.opaacity = 1
+    progress.style.width = progress.getAttribute('data-done') + '%'
+    progress.style.opacity = 1
 }, 500)
